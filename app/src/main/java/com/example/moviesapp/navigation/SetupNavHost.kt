cc770175b0ab07c3c0068ed60d884c451b0b2b42
Dev_Elvin <elvin_66635@mail.ru>
2022-08-15 14:48:11 +0400

    splash screen has added

--- app/src/main/java/com/example/moviesapp/navigation/SetupNavHost.kt
+++ app/src/main/java/com/example/moviesapp/navigation/SetupNavHost.kt
@@ -1,7 +1,6 @@
 package com.example.moviesapp.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -25,8 +24,6 @@ fun SetupNavHost(navController: NavHostController) {
             MainScreen()
         }
         composable(route = Screens.Details.route) {
-
         }
-
     }
 }
